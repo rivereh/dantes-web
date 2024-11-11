565524f8dab3c292f0d2f5a8625d5a30929ce896
rivereh <riverehill@gmail.com>
2024-11-11 02:34:30 -0800

    Add office scene and fix teleports

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,11 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
 
-    [SerializeField] float speed = 5f;
-    
+    [SerializeField] float speed;
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 10f;
+
+    bool sprinting;
 
 
     void Start()
@@ -31,6 +34,9 @@ public class Player : MonoBehaviour
 
     void Move()
     {
+        sprinting = Input.GetKey(KeyCode.LeftShift);
+        speed = sprinting ? runSpeed : walkSpeed;
+
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         Vector2 playerVelocity = new Vector2(moveInput.x * speed, rb.velocity.y);
         rb.velocity = playerVelocity;
